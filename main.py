26a130b7abcc89ab7b74a503176c544226476a98
antonio vazquez <avazquez@arete.ws>
2021-04-27 17:44:04 -0500

    estable
    insercion y lectura de la base de datos

--- main.py
+++ main.py
@@ -9,7 +9,14 @@ from googleapiclient.discovery import build
 import pymysql
 from pymysql.err import OperationalError
 from pymysql.constants import CLIENT
+from . import schemas, models
+from database import engine
 
+# models.Base.metadata.create_all(engine)
+
+CONFIG_BUCKET=None
+SQL_CONFIG=None
+mysql_conn = None
 
 class Blog(BaseModel):
     title: str
@@ -17,14 +24,12 @@ class Blog(BaseModel):
     published: bool
 
 
-class Almacen(BaseModel):
+class Spreadsheet(BaseModel):
     spreadsheet_id_input: str
     range_input: str
-    spreadsheet_id_ouput: str
-    range_ouput: str
 
 
-app = FastAPI()
+app = FastAPI(title='Apis Demo',description='apis de prueba de uso de spreadsheets con FastApi')
 
 
 @app.get('/blog')
@@ -55,7 +60,6 @@ def root():
     return {'hola mundo': 1}
 
 
-
 # =============================================================
 
 # --------------------------------------------------------------------
@@ -84,11 +88,25 @@ def getLocalCredentials():
     return credential
 
 
-#--------------------------------------------------------------------
+def getLocalCredentialsByName(CREDENTIAL_FILE):
+    # Obtiene los permisos de un archivo en la computadora
+    path_file = '{path}/{file}'.format(path=os.environ.get('CREDENTIALS'), file=CREDENTIAL_FILE)
+    print('LOCAL path_file', path_file)
+    credential = service_account.Credentials.from_service_account_file(path_file)
+    print('LOCAL project_id', credential.project_id)
+    return credential
+
+
+def getLocalFileByName(FileName):
+    path_file = '{path}/{file}'.format(path=os.environ.get('CREDENTIALS'), file=FileName)
+    return open(path_file).read()
+
+
+# --------------------------------------------------------------------
 # obtiene credenciales de un json guardado el storage
 # este service account es el que tiene permiso de lectura/escritura a los Sheets
 # spreadsheet-writer@arete-almacenes.iam.gserviceaccount.com
-#--------------------------------------------------------------------
+# --------------------------------------------------------------------
 def getCredentialsSheets():
     # blob: apuntador al archivo de configuración SQL
     config_file_name = 'credentials.json'
@@ -98,62 +116,167 @@ def getCredentialsSheets():
     # print(parsed_json_creds)
     # print('--------------------------- credentials ------------------------------------------- END')
     # build credentials with the service account dict
-    scopes = ['https://www.googleapis.com/auth/drive','https://www.googleapis.com/auth/spreadsheets']
-    credentials = service_account.Credentials.from_service_account_info(parsed_json_creds, scopes=scopes,)
+    scopes = ['https://www.googleapis.com/auth/drive', 'https://www.googleapis.com/auth/spreadsheets']
+    credentials = service_account.Credentials.from_service_account_info(parsed_json_creds, scopes=scopes, )
     print('CONNECT', credentials.project_id)
     return credentials
 
-#--------------------------------------------------------------------
+
+# --------------------------------------------------------------------
 # obtiene datos de conexion a bd
 # de un archivo guardado en storage
-#--------------------------------------------------------------------
+# --------------------------------------------------------------------
 def getConfigBD():
     # blob: apuntador al archivo de configuración SQL
     config_file_name = 'config-sql.txt'
-    contents = CONFIG_BUCKET.get_blob(config_file_name).download_as_string().splitlines()
-    # Initialize mysql config
-    SQL_CONNECTION_NAME = contents[1].decode("utf-8")
-    SQL_CONFIG = {
-      'user': contents[7].decode("utf-8"),
-      'password': contents[19].decode("utf-8"),
-      'db': contents[4].decode("utf-8"),
-      'charset': 'utf8',
-      'port': 3306,
-      'host': contents[16].decode("utf-8"),
-      'cursorclass': pymysql.cursors.DictCursor,
-      'autocommit': True,
-      'client_flag': CLIENT.FOUND_ROWS
-    }
+    if os.environ.get('LOCAL'):
+        contents = getLocalFileByName(config_file_name).splitlines()
+    else:
+        contents = CONFIG_BUCKET.get_blob(config_file_name).download_as_string().splitlines()
+    SQL_CONFIG = format_connection(contents)
+    return SQL_CONFIG
+
+def getConfigBDDos():
+    config_file_name = 'config-sql.txt'
+    if os.environ.get('LOCAL'):
+        contents = getLocalFileByName(config_file_name).splitlines()
+    else:
+        contents = CONFIG_BUCKET.get_blob(config_file_name).download_as_string().splitlines()
+    SQL_CONFIG = format_connection_dos(contents)
     return SQL_CONFIG
 
 
-#---------------------------------------------------------------------
-# conexion al bucket de configuración
-#---------------------------------------------------------------------
-print('creating instance of buckets')
-CONFIG_BUCKET = getObjBucket()
-print('creating instance of bd')
-# SQL_CONFIG = getConfigBD()
-print('loading ok')
-#==============================================================
+def format_connection_dos(contents):
+    if not os.environ.get('LOCAL'):
+        SQL_CONFIG = {
+            'user': contents[7].decode("utf-8"),
+            'password': contents[19].decode("utf-8"),
+            'db': contents[4].decode("utf-8"),
+            'charset': 'utf8',
+            'port': 3306,
+            'host': contents[16].decode("utf-8"),
+            'cursorclass': pymysql.cursors.DictCursor,
+            'autocommit': True,
+            'client_flag': CLIENT.FOUND_ROWS
+        }
+    else:
 
-@app.post('/crear_almacen')
-def crear_almacen(almacen: Almacen):
+        SQL_CONFIG = {
+            'user': contents[7],
+            'password': contents[19],
+            'db': contents[4],
+            'charset': 'utf8',
+            'port': 3306,
+            'host': contents[16],
+            'cursorclass': pymysql.cursors.DictCursor,
+            'autocommit': True,
+            'client_flag': CLIENT.FOUND_ROWS
+        }
+    return SQL_CONFIG
+
+
+
+def format_connection(contents):
+    if not os.environ.get('LOCAL'):
+        SQL_CONNECTION_NAME = contents[1].decode("utf-8")
+        SQL_CONFIG = {
+            'user': contents[7].decode("utf-8"),
+            'password': contents[19].decode("utf-8"),
+            'db': contents[4].decode("utf-8"),
+            'charset': 'utf8',
+            'port': 3306,
+            'host': contents[16].decode("utf-8"),
+            'cursorclass': pymysql.cursors.DictCursor,
+            'autocommit': True,
+            'client_flag': CLIENT.FOUND_ROWS
+        }
+    else:
+        SQL_CONNECTION_NAME = contents[1]
+        SQL_CONFIG = {
+            'user': contents[7],
+            'password': contents[19],
+            'db': contents[4],
+            'charset': 'utf8',
+            'port': 3306,
+            'host': contents[16],
+            'cursorclass': pymysql.cursors.DictCursor,
+            'autocommit': True,
+            'client_flag': CLIENT.FOUND_ROWS
+        }
+    return SQL_CONFIG
+
+@app.on_event("startup")
+async def startup():
+    print('creating instance of buckets')
+    if os.environ.get('LOCAL'):
+        CONFIG_BUCKET = None
+    else:
+        CONFIG_BUCKET = getObjBucket()
+    SQL_CONFIG = getConfigBD()
+
+
+
+
+def __get_cursor():
+    """
+    Helper function to get a cursor
+      PyMySQL does NOT automatically reconnect,
+      so we must reconnect explicitly using ping()
+    """
+    try:
+        return mysql_conn.cursor()
+    except OperationalError:
+        mysql_conn.ping(reconnect=True)
+        return mysql_conn.cursor()
+
+
+# ==============================================================
+
+
+
+@app.post('/escritura_almacen')
+def crear_almacen(almacen: Spreadsheet):
     spreadsheet_id_input = almacen.spreadsheet_id_input
-    spreadsheet_id_ouput=almacen.spreadsheet_id_ouput
     range_input = almacen.range_input
-    range_ouput=almacen.range_ouput
-
-    credentials = getCredentialsSheets()
+    if os.environ.get('LOCAL'):
+        credentials = getLocalCredentialsByName('arete-almacenes-spreadsheets.json')
+    else:
+        credentials = getCredentialsSheets()
     service = build('sheets', 'v4', credentials=credentials)
     response = service.spreadsheets().values().get(spreadsheetId=spreadsheet_id_input, range=range_input).execute()
-    response['range'] = range_ouput
-    request2 = service.spreadsheets().values().update(spreadsheetId=spreadsheet_id_ouput, range=range_ouput, body=response, valueInputOption='RAW')
-    response2 = request2.execute()
+    print(response['values'])
 
+    try:
+        mysql_conn = pymysql.connect(**SQL_CONFIG)
+    except OperationalError:
+        print('error de conexion')
+
+    with mysql_conn.cursor() as cursor:
+        resp = cursor.execute('select * from inventario')
+
+        sql = "INSERT INTO inventario(inventario_nombre,inventario_telefono,inventario_correo,inventario_direccion) VALUES "
+        val = []
+        for row in response['values']:
+            val.append(f"(\'{row[0]}\', \'{row[1]}\', \'{row[2]}\', \'{row[3]}\')")
+
+        insert=sql+','.join(val)
+        cursor.execute(insert)
     return {'data': 'almacen creado'}
 
 
+@app.post('/lectura_almacen')
+def lectura_almacen():
+    try:
+        mysql_conn = pymysql.connect(**SQL_CONFIG)
+    except OperationalError:
+        print('error de conexion')
+
+    with mysql_conn.cursor() as cursor:
+        cursor.execute('select * from inventario')
+        rows = []
+        for row in cursor.fetchall():
+            rows.append(row)
+    return {'data': rows}
 
 
 if __name__ == "__main__":
